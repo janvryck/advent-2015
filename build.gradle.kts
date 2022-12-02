3a31b4dc511785a183247aeb65f11b44a67c18e4
Jan Van Ryckeghem <jan@tabs-spaces.be>
2022-12-02 15:16:00 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.20"
+    application
+}
+
+group = "be.tabs_spaces"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.slf4j:slf4j-simple:2.0.5")
+    implementation("org.reflections:reflections:0.10.2")
+
+    testImplementation("org.assertj:assertj-core:3.23.1")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("be.tabs_spaces.advent.Runner")
+}
