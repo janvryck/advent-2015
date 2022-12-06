87943384ff6d3312b1f40e5e0a4b0b8f1aafacc8
Jan Van Ryckeghem <jan@tabs-spaces.be>
2022-12-06 11:03:12 +0100

    Day 04 - refactor - still brute-forced

--- src/main/kotlin/be/tabs_spaces/advent/days/Day04.kt
+++ src/main/kotlin/be/tabs_spaces/advent/days/Day04.kt
@@ -2,26 +2,25 @@ package be.tabs_spaces.advent.days
 
 import java.security.MessageDigest
 
-class Day04: Day(4) {
+class Day04 : Day(4) {
+
+    companion object {
+        private val MD5 = MessageDigest.getInstance("MD5")
+    }
+
     override fun partOne() = mineAdventCoin(inputString, leadingZeroes = 5)
 
     override fun partTwo() = mineAdventCoin(inputString, leadingZeroes = 6)
 
-    fun mineAdventCoin(input: String, leadingZeroes: Int): Any {
-        var suffix = 0L
-        while(!"$input${suffix}".md5().startsWith("0".repeat(leadingZeroes))) {
-            suffix++
-        }
-        return suffix
-    }
+    fun mineAdventCoin(input: String, leadingZeroes: Int) = generateSequence(0L) { it + 1 }
+        .takeWhile { !"$input$it".mined(leadingZeroes) }
+        .last() + 1
 
-    private fun String.md5(): String {
-        with(MessageDigest.getInstance("MD5")) {
-            return digest(this@md5.toByteArray()).joinToString("") {
-                String.format("%02x", it)
-            }
-        }
-    }
+    private fun String.mined(leadingZeroes: Int) = md5().startsWith("0".repeat(leadingZeroes))
+
+    private fun String.md5(): String = MD5.digest(toByteArray()).toHex()
+
+    private fun ByteArray.toHex() = joinToString("") { String.format("%02x", it) }
 
 
 }
