96067f7fe80dc4856e533cac5ec0eb178cb0d71d
Jan Van Ryckeghem <jan@tabs-spaces.be>
2022-12-06 13:15:24 +0100

    Day 04 - refactor - still brute-forced

--- src/main/kotlin/be/tabs_spaces/advent/days/Day04.kt
+++ src/main/kotlin/be/tabs_spaces/advent/days/Day04.kt
@@ -13,12 +13,13 @@ class Day04 : Day(4) {
     override fun partTwo() = mineAdventCoin(inputString, leadingZeroes = 6)
 
     fun mineAdventCoin(input: String, leadingZeroes: Int) = generateSequence(0L) { it + 1 }
-        .takeWhile { !"$input$it".mined(leadingZeroes) }
-        .last() + 1
-
-    private fun String.mined(leadingZeroes: Int) = md5().startsWith("0".repeat(leadingZeroes))
+        .map { "$input$it".md5() }
+        .takeWhile { !it.mined(leadingZeroes) }
+        .count()
 
     private fun String.md5(): String = MD5.digest(toByteArray()).toHex()
+
+    private fun String.mined(leadingZeroes: Int) = startsWith("0".repeat(leadingZeroes))
 
     private fun ByteArray.toHex() = joinToString("") { String.format("%02x", it) }
 
